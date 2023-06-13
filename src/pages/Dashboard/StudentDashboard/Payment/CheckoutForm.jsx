dbc41fce94d87613eaf0f85a86c903f9a35b391b
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-06-13 23:37:04 +0600

    payment done

--- src/pages/Dashboard/StudentDashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/StudentDashboard/Payment/CheckoutForm.jsx
@@ -2,23 +2,35 @@ import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import { useContext, useEffect, useState } from 'react';
 import useAxiosSecure from '../../../../api/useAxiosSecure';
 import { AuthContext } from '../../../../provider/AuthProvider';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
-const CheckoutForm = ({price}) => {
+const CheckoutForm = ({ item }) => {
     const { user } = useContext(AuthContext);
     const stripe = useStripe();
     const elements = useElements()
     const [cardError, setCardError] = useState('');
     const [axiosSecure] = useAxiosSecure()
     const [clientSecret, setClientSecret] = useState("");
+    const [transactionId, setTransactionId] = useState('');
+    const [processing, setProcessing] = useState(false);
+    const navigate = useNavigate()
+
+    const { Product_id, _id, classImage, available_seats, number_of_students, className, price, user_email, } = item;
+
+    const paymentInfo = {
+        Product_id, paymentId: _id, classImage, className, price, user_email, date: new Date()
+
+    }
 
     useEffect(() => {
         console.log([price]);
-        if(price > 0){
+        if (price > 0) {
             axiosSecure.post("/create-payment-intent", { price })
-            .then(res => {
-                console.log(res.data.clientSecret);
-                setClientSecret(res.data.clientSecret)
-            })
+                .then(res => {
+                    console.log(res.data.clientSecret);
+                    setClientSecret(res.data.clientSecret)
+                })
         }
 
     }, [price, axiosSecure])
@@ -51,6 +63,7 @@ const CheckoutForm = ({price}) => {
 
             setCardError('');
         }
+        setProcessing(true)
 
         const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
             clientSecret,
@@ -66,6 +79,47 @@ const CheckoutForm = ({price}) => {
         );
         if (confirmError) {
             console.log(confirmError);
+        }
+       
+        setProcessing(false)
+
+        if (paymentIntent.status === 'succeeded') {
+            setTransactionId(paymentIntent.id);
+            const payment = {...paymentInfo, transactionId: transactionId }
+            axiosSecure.post('/payments', payment)
+                .then(res => {
+                    console.log(res);
+
+                    axiosSecure.delete(`/deleteBookings/${_id}`)
+                        .then(deleteRes => {
+                            console.log(deleteRes);
+                            const updatedSeats = parseInt(available_seats) - 1;
+                            const updatedStudents = parseInt(number_of_students) + 1;
+
+                            axiosSecure.patch('/updateSomeInfo', {
+                                Product_id,
+                                available_seats: updatedSeats,
+                                number_of_students: updatedStudents
+                            })
+                                .then(updateRes => {
+                                    console.log(updateRes);
+
+                                    navigate('/dashboard/payment-history')
+                                    toast.success('Class Successfully paid');
+
+                                })
+                                .catch(updateErr => {
+                                    console.log(updateErr);
+                                    toast.error('Failed to update class information');
+                                });
+                        })
+                        .catch(deleteErr => {
+                            console.log(deleteErr);
+                            toast.error('Failed to delete booking');
+                        });
+                })
+                .catch(err => console.log(err));
+
         }
     }
     return (
@@ -87,12 +141,14 @@ const CheckoutForm = ({price}) => {
                         },
                     }}
                 />
-                <button type="submit" className='bg-pink-500 border-black rounded-lg w-24'disabled={!stripe || !clientSecret }>
+               <div className='w-full flex justify-center mt-5'>
+               <button type="submit" className='bg-pink-500 border-black text-white rounded-lg w-36 p-2 mt-5 ' disabled={!stripe || !clientSecret || processing}>
                     Pay
                 </button>
+               </div>
             </form>
             {cardError && <p className="text-red-600 ml-8">{cardError}</p>}
-            {/* {transactionId && <p className="text-green-500">Transaction complete with transactionId: {transactionId}</p>} */}
+            {transactionId && <p className="text-green-500">Transaction complete with transactionId: {transactionId}</p>}
         </>
     );
 };
