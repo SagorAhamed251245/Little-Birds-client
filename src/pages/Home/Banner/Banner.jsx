54b8af6a3380c7185d930d04be909cb132e2683d
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-06-19 15:26:36 +0600

    banner section improved

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,18 +1,58 @@
-import banner1 from '../../../assets/banner1.jpg'
-import banner2 from '../../../assets/banner2.jpg'
-import banner3 from '../../../assets/banner3.jpg'
-import banner4 from '../../../assets/banner4.jpg'
-import Carousel from './Carousel'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import banner1 from "../../../assets/banner1.jpg";
+import banner2 from "../../../assets/banner2.jpg";
+import banner3 from "../../../assets/banner3.jpg";
+import banner4 from "../../../assets/banner4.jpg";
+import { Navigation } from "swiper";
+import { Zoom } from "react-awesome-reveal";
 
-
+const banner = [
+  {
+    img: banner1,
+    title: "Unleash Your Inner Artist at our Summer Art Adventure!"
+  },
+  {
+    img: banner2,
+    title: "Get Creative in the Sun at our Summer Art Camp!"
+  },
+  {
+    img: banner3,
+    title: "Discover the Artistic Wonders of Summer at our Creative Camp!"
+  },
+  {
+    img: banner4,
+    title: "Make Art, Make Friends, and Make Memories at our Summer Camp!"
+  },
+];
 
 export const Banner = () => {
+  return (
+    <>
+      <div className="carousel w-full h-[500px] ">
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {banner.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div className=" flex relative w-full  h-[500px]">
+                <div className="w-full flex">
+                  <img
+                    src={item.img}
+                    className="w-1/2 object-left  object-cover "
+                  />
 
-    return (
-        <>
-            <div className="carousel w-full h-[500px] ">
+                  <div className="bg-pink-500 w-1/2 h-full flex justify-center items-center text-white font-Dosis">
+                    <p className=" text-3xl lg:text-6xl md:text-5xl  font-bold w-full md:w-9/12 text-center ">
+                      <Zoom delay={1e3}>{item.title}</Zoom>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-                <Carousel
+        {/* <Carousel
                     photo={banner1}
                     ids={'slide1'}
                     slid_right={'#slide4'}
@@ -39,9 +79,8 @@ export const Banner = () => {
                     slid_right={'#slide3'}
                     slid_left={'#slide1'}
                     bannerTitle={'Make Art, Make Friends, and Make Memories at our Summer Camp!'}
-                ></Carousel>
-
-            </div>
-        </>
-    );
-}
+                ></Carousel> */}
+      </div>
+    </>
+  );
+};
--- src/pages/Home/Banner/Carousel.jsx
+++ src/pages/Home/Banner/Carousel.jsx
@@ -1,23 +1,18 @@
-import {  Zoom} from "react-awesome-reveal";
-const Carousel = ({photo, ids, slid_right, slid_left, bannerTitle}) => {
-    return (
-        <div id={ids} className="carousel-item relative w-full">
-        <div className='w-full flex'>
-            
-            <img src={photo} className="w-1/2 object-left  object-cover " />
-            
-            <div className='bg-pink-500 w-1/2 h-full flex justify-center items-center text-white font-Dosis'>
-                
-                <p className=" text-3xl lg:text-6xl md:text-5xl  font-bold w-full md:w-9/12 text-center "><Zoom delay={1e3} >{bannerTitle}</Zoom></p>
-                
-                </div>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href={slid_right} className="btn btn-circle">❮</a>
-            <a href={slid_left} className="btn btn-circle">❯</a>
-        </div>
+import { Zoom } from "react-awesome-reveal";
+const Carousel = ({ photo, ids, slid_right, slid_left, bannerTitle }) => {
+  return (
+    <div id={ids} className="carousel-item relative w-full">
+      
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href={slid_right} className="btn btn-circle">
+          ❮
+        </a>
+        <a href={slid_left} className="btn btn-circle">
+          ❯
+        </a>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Carousel;
